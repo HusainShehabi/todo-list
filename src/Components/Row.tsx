0f5efe5e30465d3fd8534cc7edb6075b95d06409
HusainShehabi <shehabi.hussain@gmail.com>
2022-02-13 10:07:54 +0300

    implemented check handler

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -8,17 +8,19 @@ type Todo = {
 type TodoProps = {
    todo: Todo
    handleDeleteTodo: (id: string) => void
+   handleCheckTodo: (id: string) => void
 }
 
 export const Row = ({
      todo: { task, isCompleted, id },
      handleDeleteTodo,
+     handleCheckTodo,
      }: TodoProps) => (
     <div>
         <p>{task}</p>
         <div>
         <button aria-label="Delete a todo" onClick={() => handleDeleteTodo(id)}>X</button>
-        <input type="checkbox" checked={isCompleted} onChange={() => null}></input>
+        <input type="checkbox" checked={isCompleted} onChange={() => handleCheckTodo(id)}></input>
         </div>
     </div>
 )
--- src/Components/Todos.tsx
+++ src/Components/Todos.tsx
@@ -13,14 +13,27 @@ export const Todos = () => {
     const [todos, setTodos] = useState<Todo[]>(data)
 
     const handleDeleteTodo = (id: string) => {
-        const updatedTodos = todos.filter((todo) => todo.id != id)//loop through all todos and filter id that got clicked
+        const updatedTodos = todos.filter((todo) => todo.id !== id)//loop through all todos and filter id that got clicked
         setTodos(updatedTodos)//update state to new todos
+    }
+
+    const handleCheckTodo = (id: string) => {
+        const updatedTodos = todos.map((todo ) => {
+            if(todo.id === id){
+                return{
+                    ...todo,
+                    isCompleted: !todo.isCompleted,
+                }
+            }
+            return todo
+        })
+        setTodos(updatedTodos)
     }
 
     return(
         <section>
             {todos.map((todo) =>(
-                <Row key={todo.id} todo={todo} handleDeleteTodo={handleDeleteTodo}/>
+                <Row key={todo.id} todo={todo} handleDeleteTodo={handleDeleteTodo} handleCheckTodo={handleCheckTodo}/>
             ))}
         </section>
     )
